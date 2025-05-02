11abc574ae7cf8d1a6a3b96081b3a13c6cc41963
Vladislav Solokhin <47882145+mrvssq@users.noreply.github.com>
2025-05-02 19:49:02 +0300

    419 стр

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -52,6 +52,7 @@ func (app *application) render(w http.ResponseWriter, status int, page string,
 func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
 		CurrentYear: time.Now().Year(),
+		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
 	}
 }
 
